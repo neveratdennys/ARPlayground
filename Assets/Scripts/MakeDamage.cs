a2a684a84a549593d07036a8e95d987a661ae4dc
neveratdennys <xibu.ubc@gmail.com>
2023-09-30 21:32:13 -0600

    First project milestone

---
+++ Assets/Scripts/MakeDamage.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MakeDamage : MonoBehaviour
+{
+    [SerializeField] private int _damagePower = 10;
+    [SerializeField] private string _tagToCompare = "Player";
+    [SerializeField] private UnityEvent OnTrigger;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(_tagToCompare))
+        {
+            if (other.TryGetComponent(out Health health))
+            {
+                if (health.enabled)
+                {
+                    health.ReceiveDamage(_damagePower);
+                    OnTrigger?.Invoke();    
+                }
+            }
+        }
+    }
+}
